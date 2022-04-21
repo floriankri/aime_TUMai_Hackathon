0199c83523f8fab97b72955022649924c9ef41dc
Malte von Ehren <malte.von.ehren@tum.de>
2022-04-21 14:06:19 +0200

    generate new type of graph

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,6 @@
 from .hpo import HPO, HPOEntry
 from .obo import read_hpo_from_obo
-from .viz import make_graph_to_depth
+from .viz import make_graph_to_depth, make_graph_2
 
-__all__ = ['HPO', 'HPOEntry', 'read_hpo_from_obo', 'make_graph_to_depth']
+__all__ = ['HPO', 'HPOEntry', 'read_hpo_from_obo',
+           'make_graph_to_depth', 'make_graph_2']
--- utils/viz.py
+++ utils/viz.py
@@ -1,5 +1,6 @@
+from typing import Optional
 from graphviz import Digraph
-from .hpo import HPOEntry
+from .hpo import HPOEntry, HPO
 
 
 def make_graph_to_depth(node: HPOEntry, depth: int) -> Digraph:
@@ -14,9 +15,9 @@ def safe_id(id: str):
     return id.replace(':', '_')
 
 
-def _add_node(g: Digraph, entry: HPOEntry):
+def _add_node(g: Digraph, entry: HPOEntry, color: str = "white"):
     g.node(safe_id(entry.id),
-           f'<{entry.id}<br/><FONT POINT-SIZE="8">{entry.name}</FONT>>', shape="box")
+           f'<{entry.id}<br/><FONT POINT-SIZE="8">{entry.name}</FONT>>', shape="box", style="filled", fillcolor=color)
 
 
 def _graph_to_depth(g: Digraph, node: HPOEntry, depth: int):
@@ -27,3 +28,29 @@ def _graph_to_depth(g: Digraph, node: HPOEntry, depth: int):
     for child in node._children:
         _graph_to_depth(g, child, depth-1)
         g.edge(safe_id(node.id), safe_id(child.id))
+
+
+def make_graph_2(hpo: HPO, labevents: list[str], diagnoses: list[str],
+                 labevent_color: str = "green", diagnoses_color: str = "yellow") -> Digraph:
+    'generates a graph displaying the labevents and diagnoses'
+    g = Digraph()
+    e: set[str] = set()  # a set to keep track of the nodes added already
+
+    def _add_upwards(node: HPOEntry, color: Optional[str] = None):
+        'adds `node` and all of its parents (if they are not in `e`)'
+        assert node.id not in e
+        if color:
+            _add_node(g, node, color)
+        else:
+            _add_node(g, node)
+        e.add(node.id)
+        for parent in node._parents:
+            if parent.id not in e:
+                _add_upwards(parent)
+            g.edge(safe_id(parent.id), safe_id(node.id))
+
+    for id in labevents:
+        _add_upwards(hpo.entries_by_id[id], labevent_color)
+    for id in diagnoses:
+        _add_upwards(hpo.entries_by_id[id], diagnoses_color)
+    return g
