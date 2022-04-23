80a79013468f5831046a7404ee79b29ef965c0fc
Robin Frasch <robin.frasch@tum.de>
2022-04-23 23:11:07 +0200

    Comments

--- nn_files/fcn_model.py
+++ nn_files/fcn_model.py
@@ -4,6 +4,11 @@ from .autoencoder import Autoencoder
 
 
 class FCNModel(nn.Module):
+    """
+    Structure for Fully Connected Network Model
+    The pretrained Autoencoder is an optional input, separation in the code makes it possible
+    to use different architectures, depending on if a pretrained encoder is in place or not
+    """
     def __init__(
         self,
         input_size: int, hidden_size: int, output_size: int,
@@ -12,15 +17,20 @@ class FCNModel(nn.Module):
         dropOutRatio: float = 0,
     ):
         super().__init__()
-
+        
+        
+        # model dimensions
         self.NN = nn.Identity()
         self.input_size = input_size
         self.hidden_size = hidden_size
         self.output_size = output_size
+        
+        # enlarging factor denotes the increase or decrease in size between input and hidden layer
         self.enlarging_factor = enlarging_factor
         if autoencoder:
             self.encoder = autoencoder.return_encoder()
 
+            # architecture and initialization of the FCN
             l1 = nn.Linear(self.input_size, self.hidden_size)
             nn.init.xavier_uniform_(l1.weight)
             d1 = nn.Dropout(p=dropOutRatio)
@@ -30,7 +40,7 @@ class FCNModel(nn.Module):
             l3 = nn.Linear(self.hidden_size, self.output_size)
             nn.init.xavier_uniform_(l3.weight)
 
-            # define model architecture and move to cuda
+            
             self.NN = nn.Sequential(
                 l1,
                 nn.Tanh(),
@@ -45,6 +55,7 @@ class FCNModel(nn.Module):
         else:
             self.encoder = None
 
+            # architecture and initialization of the FCN
             l1 = nn.Linear(self.input_size, self.hidden_size)
             nn.init.xavier_uniform_(l1.weight)
             d1 = nn.Dropout(p=dropOutRatio)
@@ -54,7 +65,7 @@ class FCNModel(nn.Module):
             l3 = nn.Linear(self.hidden_size, self.output_size)
             nn.init.xavier_uniform_(l3.weight)
 
-            # define model architecture and move to cuda
+            
             self.NN = nn.Sequential(
                 l1,
                 nn.Tanh(),
@@ -67,6 +78,9 @@ class FCNModel(nn.Module):
             )
 
     def forward(self, x):
+        """
+        If an encoder is in place, the input for the FCN should first be sent through the encoder
+        """
         if self.encoder:
             x = self.encoder(x)
         x = self.NN(x)
